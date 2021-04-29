0ea9e573021d6580781305349987cea2510fc357
SecretCat <luckyhiroaki@icloud.com>
2021-04-29 09:59:21 +0000

    update

--- plugin/plgmaker.vim
+++ plugin/plgmaker.vim
@@ -1,4 +1,2 @@
 scriptencoding utf-8
 
-command! -nargs=+ Mkplg call plgmaker#MakePlugin(<f-args>)
-
--- rplugin/python3/plgmaker/__init__.py
+++ rplugin/python3/plgmaker/__init__.py
@@ -3,15 +3,20 @@ import pynvim
 
 @pynvim.plugin
 class PluginMaker(object):
-
     def __init__(self, nvim):
         self.nvim = nvim
 
-#   @pynvim.command('TestCommand', nargs='*', range='')
-#   def testcommand(self, args, range):
-#       self.nvim.current.line = ('Command with args: {}, range: {}'.format(args, range))
+#    @pynvim.command('TestCommand', nargs='*', range='')
+#    def testcommand(self, args, range):
+#        self.nvim.current.line = ('Command with args: {}, range: {}'.format(args, range))
+#
+#    @pynvim.function('TestFunction', sync=True)
+#    def testfunction(self, args):
+#        self.nvim.out_write(self.nvim.current.line + "\n")
+    
+    @pynvim.command('MkPlug', nargs='*')
+    def makeplugin(self, args):
+        self.nvim.out_write(str(type(args)) + '\n')
+        for i in args:
 
-    @pynvim.command('Mkplug', nargs='*')
-    def makePlugin(self, args):
-       T=(type(args))
-       self.nvim.out_write=("hoge\n")
+
